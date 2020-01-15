ac1833ba8ab3410b107194e3d37fcacd63b3d384
Dimitre <dimitre@users.noreply.github.com>
2020-01-14 22:28:31 -0300

    up

---
+++ up.sh
@@ -0,0 +1,4 @@
+yum --enablerepo=nginx -y install nginx
+yum --enablerepo=mariadb -y install MariaDB-server MariaDB-client
+yum --enablerepo=remi-php74 -y install php-common php-fpm php-gd php-mysqlnd php-pdo php-pecl-jsonc php-pecl-zip php-xml php-fpm
+
