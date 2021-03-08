f778124b735d36df018a94e9327092577125aab2
Dimitre <dimitre@users.noreply.github.com>
2021-03-08 01:50:50 -0300

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
 yum --enablerepo=nginx -y install nginx
 yum --enablerepo=mariadb -y install MariaDB-server MariaDB-client
-yum --enablerepo=remi-php74 -y install php-common php-fpm php-gd php-mysqlnd php-pdo php-pecl-jsonc php-pecl-zip php-xml php-fpm
+yum --enablerepo=remi-php80 -y install php-common php-fpm php-gd php-mysqlnd php-pdo php-pecl-jsonc php-pecl-zip php-xml php-fpm
 
